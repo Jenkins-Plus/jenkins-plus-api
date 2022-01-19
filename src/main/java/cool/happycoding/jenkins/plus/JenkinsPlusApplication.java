3ea9e27d7666e8a0f6eac5c739f30715dcab38cc
pengzhenchen <pzsoftchen@163.com>
2022-01-19 13:39:08 +0800

    init: init project

---
+++ src/main/java/cool/happycoding/jenkins/plus/JenkinsPlusApplication.java
@@ -0,0 +1,18 @@
+package cool.happycoding.jenkins.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description
+ *
+ * @author pengzhenchen 2022/01/19 1:25 下午
+ */
+@SpringBootApplication
+public class JenkinsPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPlusApplication.class, args);
+    }
+
+}
